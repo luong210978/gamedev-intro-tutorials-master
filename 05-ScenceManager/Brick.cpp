0d54853e1d419d80905ed9972fac1b8e91df6b5d
luong210978 <63649962+luong210978@users.noreply.github.com>
2020-10-31 22:04:43 +0700

    Add project files.

---
+++ 05-ScenceManager/Brick.cpp
@@ -0,0 +1,22 @@
+#include "Brick.h"
+#include "Sprites.h"
+CBrick::CBrick(float l, float t, float r, float b, int scene_id)
+{
+	x = l;
+	y = t;
+	width = r - l + 1;
+	height = b - t + 1;
+}
+void CBrick::Render()
+{
+	//animation_set->at(0)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
+{
+	l = x;
+	t = y;
+	r = x + width;
+	b = y + height;
+}
---
+++ 05-ScenceManager/Brick.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "GameObject.h"
+
+#define BRICK_BBOX_WIDTH  16
+#define BRICK_BBOX_HEIGHT 16
+
+class CBrick : public CGameObject
+{
+
+
+	int width;
+	int height;
+	virtual void Render();
+	virtual void GetBoundingBox(float &l, float &t, float &r, float &b);
+	public:
+		CBrick(float l, float t, float r, float b, int scene_id);
+		virtual void Render();
+		virtual void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
+#pragma once
+
